fb719c8f6389665bc092b44aa5b38f4f4fb25a9c
Kanavichev Alexey <97598151+maestroAlexey@users.noreply.github.com>
2023-04-03 12:00:36 +0300

    Add files via upload

---
+++ cookbook-main/CookBook/CookBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CookBook
+//
+//  Created by Канавичев Алексей Александрович on 3/2/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
